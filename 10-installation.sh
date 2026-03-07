7fb4b9d6b57f5366789f9127741654f95f2b7f00
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 16:37:06 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,13 +2,13 @@
 
 USERID=$(id -u) # u -> print only effective userID
 
-if ( $USERID -ne 0 ); then
+if [ $USERID -ne 0 ]; then
 echo "please run the script with root user access"
 exit 1
 fi
 echo "Installing Nginx"
 dnf install nginx -y
-if ( $? -ne 0 )
+if [ $? -ne 0 ]
 echo "Installing Nginx.............Failure"
 exit 1
 else 
