bbef6e1c27e9c83ee76e82466253c936d7b2ac97
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 13:44:52 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
-echo "script executed at :: $TIMESTAMP"
+START_TIME=$(date +%s)
+echo "script executed at :: $STAR_TTIME"
+
+sleep 10
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "script executed in : $TOTAL_TIME seconds"
