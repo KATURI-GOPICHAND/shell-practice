0ae13683f7b9e2651d3a93b3bfe9d4b2f7c02a90
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 11:17:50 +0530

    actions

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter your user name ::"
+read USER_NAME
+echo "your your user name is $USER_NAME"
+echo "please enter your password ::"
+read -S PASSWORD
+echo "your your password is $PASSWORD"
