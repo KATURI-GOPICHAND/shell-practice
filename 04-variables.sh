e9882af1730e337cd6bfc45bf17b0661c6737dc7
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 11:06:38 +0530

    actions

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,9 +3,9 @@
 PERSON1=$1 # no spaces between  = and value
 PERSON2=$2
 
-echo "PERSON1 : Hello PERSON2, how are you"
-echo "PERSON2 : Hi PERSON1,what abou you"
+echo "$PERSON1 : Hello $PERSON2, how are you"
+echo "$PERSON2 : Hi $PERSON1,what abou you"
 
 
-echo "PERSON1 : PERSON2, are you practicing DevOps"
-echo "PERSON2 : yes, how your practice is"
+echo "$PERSON1 : $PERSON2, are you practicing DevOps"
+echo "$PERSON2 : yes, how your practice is"
