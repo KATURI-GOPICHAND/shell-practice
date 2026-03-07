8dad108b2d8cd42cf419241e56223dede749dfb5
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 15:03:59 +0530

    actions

---
+++ 10-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u) # u -> print only effective userID
+
+if ( USERID -ne 0 ); then
+echo "please run the script with root user access"
+exit 1
+fi
+echo "Installing Nginx"
+dnf install nginx -y
+if ( $? -ne 0 )
+echo "Installing Nginx.............Failure"
+exit 1
+else 
+echo "Installing Nginx.............Success"
