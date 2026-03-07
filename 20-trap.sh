a90511d506ab17ee691e841f977c14f2b80624b4
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 19:13:28 +0530

    actions

---
+++ 20-trap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script/"
+LOGS_FILE="/var/log/shell-script/$0.log" # $0 -> indicates script name 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run the script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+for package in $@  # sh 14-loops.sh nginx mysql nodejs
+do 
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not istalled, installing now"
+        dnf install $package -y &>>$LOGS_FILE  
+    else
+        echo -e "$package already installed..............,$Y skipping $N"
+    fi
+done
+
+
