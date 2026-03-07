59df6dee3db379d1011ff7bbea416891879024d8
KATURI-GOPICHAND <gopichand.cloud11@gmail.com>
2026-03-07 14:20:39 +0530

    actions

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -2,6 +2,20 @@
 
 NUM1=100
 NUM2=Gopi
+NUM3=200
 
-SUM=$(($NUM1+$NUM2))
-echo "sum is: $SUM"
+SUM1=$(($NUM1+$NUM2))
+echo "sum of $NUM1 and $NUM2 is: $SUM1"
+
+
+SUM2=$(($NUM1+$NUM3))
+echo "sum of $NUM1 and $NUM2 is: $SUM2"
+
+# Array 
+FRUITS=("Apple, Banana, mango, Kiwi")
+
+echo "Fruits are : ${FRUITS[@}]}"
+echo "First Fruit is : ${FRUITS[0]}"
+echo "Second Fruit is : ${FRUITS[1]}"
+echo "Third Fruit is : ${FRUITS[2]}"
+echo "First Fruit is : ${FRUITS[3]}"
